402015348265f6fa6c83dc3b5f2c9f86f1010cc1
Peter Rosellen <2063341+prosellen@users.noreply.github.com>
2022-11-05 13:50:24 +0100

    Adds more demos

--- demo/utils/full-tree.ts
+++ demo/utils/full-tree.ts
@@ -13,6 +13,38 @@ const fullTree = [
         url: "/examples/start-level/",
         text: "Custom start level",
       },
+      {
+        url: "/examples/limit-levels/",
+        text: "Limit the depth of levels to be shown",
+        children: [
+          {
+            url: "/examples/limit-levels/level-3",
+            text: "Level 3 (dummy)",
+            redirect: "/examples/limit-levels/",
+            children: [
+              {
+                url: "/examples/limit-levels/level-4",
+                text: "Level 4 (dummy)",
+                redirect: "/examples/limit-levels/",
+                children: [
+                  {
+                    url: "/examples/limit-levels/level-5",
+                    text: "Level 5 (dummy)",
+                    redirect: "/examples/limit-levels/",
+                    children: [
+                      {
+                        url: "/examples/limit-levels/level-6",
+                        text: "Level 6 (dummy)",
+                        redirect: "/examples/limit-levels/",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
 ];
